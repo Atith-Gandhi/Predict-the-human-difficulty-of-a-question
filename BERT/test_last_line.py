b4081cf57d50ce9711425b357424ea9c3c811ff2
gandhi13 <54467326+gandhi13@users.noreply.github.com>
2021-04-30 03:56:37 +0530

    Update test_last_line.py

--- BERT/test_last_line.py
+++ BERT/test_last_line.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
-test_dataset = load_dataset('json', data_files={'test': 'quanta_test.json'}, field='questions')['test']
+test_dataset = load_dataset('json', data_files={'test': 'dataset_last_line/quanta_test.json'}, field='questions')['test']
 test_dataset = test_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
 test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
 test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
