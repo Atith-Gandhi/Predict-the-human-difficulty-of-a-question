5a84af7917473e4b90d69711d92b239c51c99437
gandhi13 <54467326+gandhi13@users.noreply.github.com>
2021-05-01 13:58:00 +0530

    Update train_full_question.py

--- BERT/train_full_question.py
+++ BERT/train_full_question.py
@@ -10,7 +10,7 @@ tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
 import random
 
 def tokenize(batch):
-    return tokenizer(batch['text'], truncation=True, max_length = 128, add_special_tokens=True, padding='max_length', return_attention_mask=True)
+    return tokenizer(batch['text'], truncation=True, max_length = 256, add_special_tokens=True, padding='max_length', return_attention_mask=True)
 
 train_dataset = load_dataset('json', data_files={'train':'dataset_full_question/quanta_train.json'}, field='questions')['train']
 train_dataset = train_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
@@ -20,7 +20,7 @@ train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label
 training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=2,
-    per_device_train_batch_size=32,
+    per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     # warmup_steps = 0,
     # weight_decay=1e-8,
