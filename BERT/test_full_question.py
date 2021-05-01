004cae934707a9feceb1f0768a91a979ef4184c9
gandhi13 <54467326+gandhi13@users.noreply.github.com>
2021-05-01 13:58:46 +0530

    Update test_full_question.py

--- BERT/test_full_question.py
+++ BERT/test_full_question.py
@@ -4,18 +4,21 @@ import torch
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def tokenize(batch):
+    return tokenizer(batch['text'], truncation=True, max_length = 256, add_special_tokens=True, padding='max_length', return_attention_mask=True)
+
 test_dataset = load_dataset('json', data_files={'test': 'dataset_full_question/quanta_test.json'}, field='questions')['test']
 test_dataset = test_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
 test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
 test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 
-model = BertForSequenceClassification.from_pretrained('models/BERT_last_line')
+model = BertForSequenceClassification.from_pretrained('models/BERT_full_question')
 
 def compute_metrics(pred):
     labels = pred.label_ids
-    print(labels)
+    # print(labels)
     preds = pred.predictions.argmax(-1)
-    print(preds)
+    # print(preds)
     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
     acc = accuracy_score(labels, preds)
     return {
