683edee593401c829e16880031d5c45edf5222e4
gandhi13 <54467326+gandhi13@users.noreply.github.com>
2021-05-01 13:59:50 +0530

    Update test_last_line.py

--- BERT/test_last_line.py
+++ BERT/test_last_line.py
@@ -4,6 +4,9 @@ import torch
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def tokenize(batch):
+    return tokenizer(batch['text'], truncation=True, max_length = 128, add_special_tokens=True, padding='max_length', return_attention_mask=True)
+    
 test_dataset = load_dataset('json', data_files={'test': 'dataset_last_line/quanta_test.json'}, field='questions')['test']
 test_dataset = test_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
 test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
