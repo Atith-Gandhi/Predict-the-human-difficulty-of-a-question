e468ad1ce18f6b2eba12f230901e587d6baffb64
gandhi13 <54467326+gandhi13@users.noreply.github.com>
2021-05-01 13:57:11 +0530

    Create test_full_question.py

---
+++ ConvBERT/test_full_question.py
@@ -0,0 +1,37 @@
+from transformers import ConvBertForSequenceClassification, ConvBertTokenizerFast, Trainer, TrainingArguments
+from nlp import load_dataset
+import torch
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+def tokenize(batch):
+    return tokenizer(batch['text'], truncation=True, max_length = 256, add_special_tokens=True, padding='max_length', return_attention_mask=True)
+
+test_dataset = load_dataset('json', data_files={'test': 'dataset_full_question/quanta_test.json'}, field='questions')['test']
+test_dataset = test_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
+test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
+test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+
+model = ConvBertForSequenceClassification.from_pretrained('models/ConvBERT_full_question')
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    # print(labels)
+    preds = pred.predictions.argmax(-1)
+    # print(preds)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+trainer = Trainer(
+    model=model,
+    compute_metrics=compute_metrics,
+    eval_dataset=test_dataset
+)
+
+trainer.evaluate()
