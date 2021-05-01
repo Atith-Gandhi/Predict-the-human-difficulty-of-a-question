244bda5d50a9f0c2c9c7f72c0ac82bbb6a02c050
Atith Gandhi <54467326+Atith-Gandhi@users.noreply.github.com>
2021-05-01 17:41:29 +0530

    Update test_last_line.py

--- BERT/test_last_line.py
+++ BERT/test_last_line.py
@@ -4,15 +4,16 @@ import torch
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+model = BertForSequenceClassification.from_pretrained('models/BERT_last_line')
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
+
 def tokenize(batch):
     return tokenizer(batch['text'], truncation=True, max_length = 128, add_special_tokens=True, padding='max_length', return_attention_mask=True)
-    
+
 test_dataset = load_dataset('json', data_files={'test': 'dataset_last_line/quanta_test.json'}, field='questions')['test']
 test_dataset = test_dataset.map(lambda example: {'label': [0 if example['difficulty'] == 'School' else 1]})
 test_dataset = test_dataset.map(tokenize, batched=True, batch_size=len(test_dataset))
 test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
-
-model = BertForSequenceClassification.from_pretrained('models/BERT_last_line')
 
 def compute_metrics(pred):
     labels = pred.label_ids
